1d1d024eb6671c342d2f9988871ce4a1931361ff
Vavar <ivhor2004@gmail.com>
2021-11-19 21:09:42 +0300

    классная работа 19.11.2021

--- Horoshev/homework1/task5.py
+++ Horoshev/homework1/task5.py
@@ -1,15 +1,15 @@
-a = 1
-b = 15
-c = 10
-d = 11
-e = 2
-max = a
-if b > max:
-    max = b
-if c > max:
-    max = c
-if d > max:
-    max = d
-if e > max:
-    max = e
-print(max)
+a = int(input())
+list_numbers = []
+
+list_numbers.append(a)
+while (a != 0):
+    a = int(input())
+    list_numbers.append(a)
+
+list_numbers.remove(0)
+min = 1000
+for number in list_numbers:
+
+    if number < min:
+        min = number
+print(min)
