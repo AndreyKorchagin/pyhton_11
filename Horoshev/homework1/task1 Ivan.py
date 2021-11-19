cfb99ae38a5a87650396cf58accc76762b256da8
Vavar <ivhor2004@gmail.com>
2021-11-19 11:53:58 +0300

    добавил дз 1

---
+++ Horoshev/homework1/task1 Ivan.py
@@ -0,0 +1,40 @@
+# номер 1
+A = int(input("Введите число: "))
+B = int(input("Введите число: "))
+while A > B:
+    print(A)
+    A -= 3
+
+# номер 4
+a = 1
+b = 15
+c  = 10
+d = 11
+e = 2
+max = a
+if b > max:
+    max = b
+if c > max:
+    max = c
+if d > max:
+    max = d
+if e > max:
+    max = e
+print(max)
+
+# номер 5
+a = 1
+b = 15
+c  = 10
+d = 11
+e = 2
+max = a
+if b > max:
+    max = b
+if c > max:
+    max = c
+if d > max:
+    max = d
+if e > max:
+    max = e
+print(max)
