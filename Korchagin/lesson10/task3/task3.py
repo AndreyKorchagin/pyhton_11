f25e4be1532d39e4a4495593c5d3828902591c22
Andrey Korchagin <Andrey Korchagin>
2022-03-02 21:09:14 +0300

    Добавил 10 урок

--- Korchagin/lesson10/task3/task3.py
+++ Korchagin/lesson10/task3/task3.py
@@ -10,11 +10,13 @@ def find_max_len(text, simbol):
             count = 0
     return max
 
+
 def replace_XXX(text, char):
     text = text.replace("XYZ", "!");
     text = text.replace("XY", "#");
     text = text.replace("X", "$");
     return text
+
 
 text = open("24_demo.txt", "r").read()
 
@@ -22,9 +24,9 @@ text = replace_XXX(text, "X")
 print(text)
 
 length = find_max_len(text, "!")
-if text.find("!"*length+"#") != -1:
+if text.find("!" * length + "#") != -1:
     print(length * 3 + 2)
-elif text.find("!"*length+"$") != -1:
+elif text.find("!" * length + "$") != -1:
     print(length * 3 + 1)
 else:
     print(length * 3)
